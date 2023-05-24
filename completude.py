d058372f4c6e70dde2282225b4362cecfb4b8faa
paulohgontijoo <paulohgontijoo@gmail.com>
2023-05-24 17:42:52 -0300

    Adicionando teste de duplicação dos campos OR Exclusivo
    Co-authored-by: joaobisi <jbisinotti@gmail.com>
    Co-authored-by: André Goretti <andremotta96@gmail.com>
    Co-authored-by: Ian Rocha <ianrocha.df@gmail.com>
    Co-authored-by: viniciused26 <viniciusoliv26@gmail.com>
    Co-authored-by: PinheiroLG <gustavo.paespinheiro@gmail.com>

--- completude.py
+++ completude.py
@@ -17,12 +17,27 @@ class Publicacao:
         self.autores = autores
 
 
+def completude_campo(campo):
+    # Verificar completude do campo
+    if campo or campo != '':
+        return True
+    return False
+
+
 def completude_registro(self):
     return 1.0
 
 
 def completude_registro_or_exclusivo(self):
-    return 1.0
+    """Função para calcular a completude de um registro OR Exclusivo
+       Retorna a quantidade de registros completos e incompletos"""
+
+    # Verificar completude dos campos aninhados OR exclusivo
+    for i in range(len(self.autores)):
+        if completude_campo(self.autores[i].lattes) != completude_campo(self.autores[i].orcid):
+            return True
+        else:
+            return False
 
 
 
--- completude_test.py
+++ completude_test.py
@@ -1,17 +1,27 @@
 import unittest
-from completude import completude_registro
+from completude import completude_registro, Publicacao, Autor, completude_registro_or_exclusivo
 
 
 class CompletudeTestCase(unittest.TestCase):
-    def test_completude_registro_falsificado(self):
-        # Falsificação: criação de testes com resultados esperados falsificados,
-        # ou seja, onde os resultados esperados são definidos previamente,
-        # mesmo que a implementação ainda não tenha sido feita.
+    #Toda publicação em porcentagem
+    def test_completude_registro(self):
         self.assertEqual(completude_registro(self), 1.0)
 
-    def test_completude_registro_or_exclusivo_falsificado(self):
-        # Falsificação: criação de testes com resultados esperados falsificados,
-        # ou seja, onde os resultados esperados são definidos previamente,
-        # mesmo que a implementação ainda não tenha sido feita.
-        self.assertEqual(completude_registro(self), 1.0)
 
+    #Espera-se um booleano sobre registros completos e incompletos
+    def test_completude_registro_or_exclusivo(self):
+        publicacao1 = Publicacao('titulo1', 'data de publicacao1', 'linguagem1',
+                                [Autor('Paulo', 'lattes1', 'orcidssspaulo', 'nationalitysssspaulo', 'birthCountryssspaulo',
+                                'birthCityssspaulo','birthStatesssspaulo')]
+                                )
+        self.assertFalse(completude_registro_or_exclusivo(publicacao1))
+
+
+    #Espera-se um booleano sobre registros completos e incompletos
+    def test_completude_registro_or_exclusivo_duplicado(self):
+        publicacao2 = Publicacao('titulo2', 'data de publicacao2', 'linguagem2',
+                                 [Autor('Jão', '', 'orcidsssJão', 'nationalityssssJão', 'birthCountrysssJão',
+                                        'birthCitysssJão', 'birthStatessssJão')]
+                                 )
+
+        self.assertTrue(completude_registro_or_exclusivo(publicacao2))
